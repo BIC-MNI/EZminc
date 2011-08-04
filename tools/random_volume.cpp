8b9aba1cfc42c924d8c07c04ac2e20e60db08d67
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2011-08-04 13:42:43 -0400

    Added gaussian noise output and storage type

--- tools/random_volume.cpp
+++ tools/random_volume.cpp
@@ -12,6 +12,7 @@
 #include <time.h>
 //#include <mt19937ar.h>
 #include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
 
 using namespace  std;
 using namespace  minc;
@@ -20,27 +21,40 @@ void show_usage (const char *name)
 {
   cerr<<"Usage:"<<name<<" <sample.mnc> <output.mnc> "<<std::endl
       <<"will generate random (uniform distribution) volume with the same parameters as sample.mnc"<<std::endl
+			<<"[--clobber] clobber the output files" << std::endl
       <<"[--min <f>] minimum value (default -1)" <<std::endl
       <<"[--max <f>] maximum value (default  1)" <<std::endl
       <<"[--mask] input volume is treated as binary mask" <<std::endl
-      <<"[--binary] random volume has binary distribution (50% of min or max)" <<std::endl;
+      <<"[--binary] random volume has binary distribution (50% of min or max)" <<std::endl
+      <<"[--float] store volume in float (default)"<<std::endl
+      <<"[--short] store volume in short"<<std::endl
+      <<"[--byte] store volume in byte"<<std::endl
+      <<"[--gauss <sigma> generate gaussian distribution with zero mean and sigma standard deviation, ignore min and max"<<std::endl;
 }
 
 int main (int argc, char **argv)
 {
+  int clobber=0;
   int mask=0,binary=0;
+	double sigma=-1.0;
   const gsl_rng_type * rng_type=gsl_rng_default;
   unsigned long int rng_seed=0;
-  
   gsl_rng * rng=NULL;
+	
+	int store_float=0,store_short=0,store_byte=0;
   
 	try
   {
     static struct option long_options[] = { 
-      {"min",  required_argument, 0, 'i'},
-      {"max",  required_argument, 0, 'a'},
+		  {"clobber", no_argument, &clobber, 1},
+      {"min",  required_argument,   0, 'i'},
+      {"max",  required_argument,   0, 'a'},
+			{"gauss",  required_argument, 0, 'g'},
       {"mask", no_argument,   &mask,   1},
       {"binary", no_argument, &binary, 1},
+      {"float", no_argument, &store_float, 1},
+			{"short", no_argument, &store_short, 1},
+			{"byte", no_argument, &store_byte, 1},
       {0, 0, 0, 0}
     };
     float min=-1.0,max=1.0;
@@ -63,6 +77,9 @@ int main (int argc, char **argv)
         case 'i':
           min=atof(optarg);
           break;
+				case 'g':
+					sigma=atof(optarg);
+					break;
         case '?':
           /* getopt_long already printed an error message. */
         default:
@@ -77,6 +94,13 @@ int main (int argc, char **argv)
     //timeval timer;
     //gettimeofday(&timer, NULL);
     //init_genrand((unsigned long)timer.tv_usec);
+    
+		if (!clobber && !access (argv[optind+1], F_OK))
+		{
+			cerr << argv[optind+1] << " Exists!" << endl;
+			return 1;
+		}
+    
     gsl_rng_env_setup();
     
     FILE *seed=fopen("/dev/urandom","rb");
@@ -106,14 +130,18 @@ int main (int argc, char **argv)
     for(int i=0;i<5;i++)
       size*=rdr.ndim(i)>0.0?rdr.ndim(i):1;
     std::vector<float> volume(size);
-    std::cout<<"Size="<<size<<std::endl;
     if(!mask)
     {
       for(std::vector<float>::iterator it=volume.begin();it!=volume.end();++it) {
-        if(!binary)
-          *it=gsl_rng_uniform(rng)*(max-min)+min;
-        else
-          *it=gsl_rng_uniform(rng)>0.5?max:min;
+				if(sigma>0.0)
+				{ 
+					*it=gsl_ran_gaussian(rng,sigma);
+				} else {
+					if(!binary)
+						*it=gsl_rng_uniform(rng)*(max-min)+min;
+					else
+						*it=gsl_rng_uniform(rng)>0.5?max:min;
+				}
       }
     }else{
       std::vector<unsigned char> mask(size);
@@ -125,17 +153,22 @@ int main (int argc, char **argv)
       {
         if(*itm) 
         {
-          if(!binary)
-            *it=gsl_rng_uniform(rng)*(max-min)+min;
-          else
-            *it=gsl_rng_uniform(rng)>0.5?max:min;
+					if(sigma>0.0)
+					{ 
+						*it=gsl_ran_gaussian(rng,sigma);
+					} else {
+						if(!binary)
+							*it=gsl_rng_uniform(rng)*(max-min)+min;
+						else
+							*it=gsl_rng_uniform(rng)>0.5?max:min;
+					}
         }else{
           *it=0.0;
         }
       }
     }
     minc_1_writer wrt;
-    wrt.open(argv[optind+1],rdr.info(),rdr.dim_no()-1,NC_FLOAT,1);
+    wrt.open(argv[optind+1],rdr.info(),rdr.dim_no()-1,store_byte?NC_CHAR:(store_short?NC_SHORT:NC_FLOAT),store_byte?0:1);
     wrt.setup_write_float();
     save_standard_volume(wrt,&volume[0]);
     
