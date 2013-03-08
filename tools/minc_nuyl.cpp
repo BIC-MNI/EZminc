880e8adb569d3f537a21f16e582b0bec8186d53d
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2013-03-08 16:54:08 -0500

    Added commulative histogram text option

--- tools/minc_nuyl.cpp
+++ tools/minc_nuyl.cpp
@@ -9,6 +9,9 @@
 
 using namespace minc;
 
+
+typedef minc::simple_commulative_histogram<float> simple_histogram;
+
 void show_usage(const char *name)
 {
   std::cerr 
@@ -18,17 +21,58 @@ void show_usage(const char *name)
       << "Medical Imaging, IEEE Transactions on , vol.19, no.2, pp.143-150, Feb. 2000 "<<std::endl
       << "http://dx.doi.org/10.1109/42.836373 "<<std::endl
       << std::endl
-      << "Usage: "<<name<<" <source> <target> [output]" << std::endl
+      << "Usage: "<<name<<" <source.mnc> <target.mnc> [output] or <source.mnc> <output.hist> --chist" << std::endl
       << "\t--source-mask <mask1.mnc>"<<std::endl
       << "\t--target-mask <mask2.mnc>"<<std::endl
       << "\t--steps <n> number of steps (default 10)"<<std::endl
       << "\t--fix_zero_padding fix mri volumes with lots of zeros in background"<<std::endl
       << "\t--verbose be verbose" << std::endl
-      << "\t--ks calculate Kolmogorov–Smirnov difference, no output is produced"<< std::endl;
+      << "\t--ks calculate Kolmogorov–Smirnov difference, no output is produced"<< std::endl
+      << "\t--chist output or target are comulative histograms" << std::endl;
+}
+
+void populate_chist(simple_histogram& hist,std::vector<double> &chist,std::vector<double> &pct,double cut_off,int steps,bool verbose=false)
+{
+  double step  = (1.0 - 2.0*cut_off/100.0) / steps ;
+
+  chist.clear();
+  pct.clear();
+  chist.reserve(steps+1);
+  pct.reserve(steps+1);
+  //provide mapping for background
+  chist.push_back(hist.min());
+  pct.push_back(0.0);
+
+  for(int i=0;i<=steps;i++)
+  {
+    double _pct =  i * step + cut_off/100.0;
+    
+    double lev_s=hist.find_percentile(_pct);
+    
+    if(lev_s-chist[chist.size()-1]<(hist.max()-hist.min())/1.0e5)
+    {
+      std::cerr<<"Warning: "<<_pct*100.0<<" percentile collapses in source, skipping"<<std::endl;
+    } else {
+      chist.push_back(lev_s);
+      pct.push_back(_pct);
+    }
+  }
+  //provide mapping for upper range
+  chist.push_back(hist.max());
+  pct.push_back(1.0);
+  
+}
+
+void save_chist(const char* output,const std::vector<double> &chist,const std::vector<double> &pct)
+{
+  std::ofstream out(output);
+  for(int i=0;i<chist.size();i++)
+  {
+    out<<pct[i]<<","<<chist[i]<<std::endl;
+  }
 }
 
 
-typedef minc::simple_commulative_histogram<float> simple_histogram;
 
 int main(int argc,char **argv)
 {
@@ -41,6 +85,7 @@ int main(int argc,char **argv)
   int clobber=0;
   int calc_ks=0;
   int fix_zero_padding=0;
+  int chist=0;
   
   double cut_off=0.01;
   
@@ -53,6 +98,7 @@ int main(int argc,char **argv)
   static struct option long_options[] =
   {
     {"verbose", no_argument, &verbose,      1},
+    {"chist",   no_argument, &chist,        1},
     {"clobber", no_argument, &clobber,      1},
     {"debug",   no_argument, &debug,        1},
     {"quiet",   no_argument, &verbose,      0},
@@ -100,15 +146,38 @@ int main(int argc,char **argv)
         return 1;
     }
   }
-
-  if ((argc - optind) < 3 && !calc_ks || (argc - optind)<2 && calc_ks)
+  
+  if( chist  )
   {
-    show_usage(argv[0]);
-    return 1;
+    if((argc - optind)<2)
+    {
+      show_usage(argv[0]);
+      return 1;
+    }
+  } else {
+    if ((argc - optind) < 3 && !calc_ks || (argc - optind)<2 && calc_ks  ) {
+      show_usage(argv[0]);
+      return 1;
+    }
   }
+  
   std::string input_src_f=argv[optind];
   std::string input_trg_f=argv[optind+1];
+  
   std::string output_f=(argc - optind)>2?argv[optind+2]:"";
+  
+  if(chist) 
+  {
+    std::cout<<"Using comulative histograms"<<std::endl;
+    
+    if((argc - optind)<3) 
+    {
+      output_f=argv[optind+1];
+      input_trg_f="";
+    } else {
+      output_f=argv[optind+2];
+    }
+  }
   
   if (!output_f.empty() && !clobber && !access (output_f.c_str(), F_OK))
   {
@@ -119,14 +188,13 @@ int main(int argc,char **argv)
   try
   {
     simple_volume<float> src,trg;
-    float src_min,src_max;
-    float trg_min,trg_max;
     
     minc_1_reader rdr1;
     rdr1.open(input_src_f.c_str());
     load_simple_volume<float>(rdr1,src);
     
     simple_histogram src_hist_s;
+    simple_histogram trg_hist_s;
     
     if(!source_mask_f.empty())
     {
@@ -146,6 +214,7 @@ int main(int argc,char **argv)
       
       if(fix_zero_padding) //will try to remove zero padding
       {
+        float src_min,src_max;
         volume_min_max(src,src_min,src_max);
         
         minc_byte_volume src_mask(src.dims());
@@ -160,123 +229,90 @@ int main(int argc,char **argv)
         src_hist_s.build_histogram(src);
       }
     }
-    
-    src_min=src_hist_s.min();
-    src_max=src_hist_s.max();
-    //src_hist.save("src.lst");
-  
-    minc_1_reader rdr2;
-    rdr2.open(input_trg_f.c_str());
-    load_simple_volume<float>(rdr2,trg);
-    simple_histogram trg_hist_s;
-    
-    if(!target_mask_f.empty())
-    {
-      minc_byte_volume   trg_mask;
-      
-      minc_1_reader rdr;
-      if(verbose) std::cout<<"loading mask:"<<target_mask_f.c_str()<<std::endl;
-      
-      rdr.open(target_mask_f.c_str());
-      load_simple_volume(rdr,trg_mask);
-      
-      if(trg_mask.size()!=trg.size())
-        REPORT_ERROR("Target mask size mismatch");
-      
-      trg_hist_s.build_histogram(trg,trg_mask);
-    } else {
-      //build histograms 
-      trg_hist_s.build_histogram(trg);
-    }
-    trg_min=trg_hist_s.min();
-    trg_max=trg_hist_s.max();
-
-    if(calc_ks)
-    {
-      if(verbose) std::cout<<"Kolmogorov–Smirnov distance:";
-      double significance=0.0;
-      double dist=src_hist_s.ks_distance(trg_hist_s,significance);
-      std::cout<<dist<<std::endl;
-      //if(verbose) std::cout<<"Significance:";
-      //std::cout<<significance<<std::endl;
-      return 0;
-    }
-    
-    
-    double step  = (1.0 - 2.0*cut_off/100.0) / steps ;
-    
     std::vector<double> src_levels_s;
-    std::vector<double> trg_levels_s;
+    std::vector<double> src_pct_s;
     
-    //provide mapping for background
-    src_levels_s.push_back(src_min);
+    populate_chist(src_hist_s,src_levels_s,src_pct_s,cut_off,steps,verbose);
     
-    trg_levels_s.push_back(trg_min);
-    
-    if(verbose)
-      std::cout<<"[ min ] "<<src_levels_s[0]<<" => "<<trg_levels_s[0]<<std::endl;
-    
-    for(int i=0;i<=steps;i++)
+    if(!input_trg_f.empty())
     {
-      double pct =  i * step + cut_off/100;
+      minc_1_reader rdr2;
+      rdr2.open(input_trg_f.c_str());
+      load_simple_volume<float>(rdr2,trg);
       
-     
-      double src_lev_s=src_hist_s.find_percentile(pct);
-      double trg_lev_s=trg_hist_s.find_percentile(pct);
       
-      if(trg_lev_s-trg_levels_s[trg_levels_s.size()-1]<(trg_max-trg_min)/100000.0)
+      if(!target_mask_f.empty())
       {
-        std::cerr<<"Warning: "<<pct*100<<" percentile collapses in target, skipping"<<std::endl;
+        minc_byte_volume   trg_mask;
+        
+        minc_1_reader rdr;
+        if(verbose) std::cout<<"loading mask:"<<target_mask_f.c_str()<<std::endl;
+        
+        rdr.open(target_mask_f.c_str());
+        load_simple_volume(rdr,trg_mask);
+        
+        if(trg_mask.size()!=trg.size())
+          REPORT_ERROR("Target mask size mismatch");
+        
+        trg_hist_s.build_histogram(trg,trg_mask);
       } else {
-        
-        src_levels_s.push_back(src_lev_s);
-        trg_levels_s.push_back(trg_lev_s);
-        
-        if(verbose)
-          std::cout<<"[ "<<pct*100.0<<" ] "<<src_levels_s[src_levels_s.size()-1]<<" => "<<trg_levels_s[trg_levels_s.size()-1]<<std::endl;
+        //build histograms 
+        trg_hist_s.build_histogram(trg);
       }
+
+      if(calc_ks)
+      {
+        if(verbose) std::cout<<"Kolmogorov–Smirnov distance:";
+        double significance=0.0;
+        double dist=src_hist_s.ks_distance(trg_hist_s,significance);
+        std::cout<<dist<<std::endl;
+        //if(verbose) std::cout<<"Significance:";
+        //std::cout<<significance<<std::endl;
+        return 0;
+      }
+      
+      
+      std::vector<double> trg_levels_s;
+      std::vector<double> trg_pct_s;
+      //provide mapping for background
+      
+      populate_chist(trg_hist_s,trg_levels_s,trg_pct_s,cut_off,steps,verbose);  
+      
+      if(verbose) 
+        std::cout<<"Recalculating intensities..."<<std::flush;
+      
+      for(int i=0;i<src.c_buf_size();i++)
+      {
+        //use LUT to map the intensities
+        int bin;
+        double input=src.c_buf()[i];
+        
+        double output=input;
+        for(bin=0;bin<src_levels_s.size();bin++)
+          if(input <= src_levels_s[bin]) break;
+        
+        if(bin==0) // first bin ?
+          output=trg_levels_s[0];
+        else if(bin>=(src_levels_s.size()-1))  
+          output=trg_levels_s[trg_levels_s.size()-1];
+        else 
+          output=(input-src_levels_s[bin-1])/(src_levels_s[bin]-src_levels_s[bin-1])*(trg_levels_s[bin]-trg_levels_s[bin-1])+trg_levels_s[bin-1];
+
+        src.c_buf()[i]=output;
+      }
+
+      if(verbose) 
+        std::cout<<"Done!"<<std::endl;
+
+      minc_1_writer wrt;
+
+      wrt.open(output_f.c_str(),rdr1);
+      wrt.append_history(history.c_str());
+
+      save_simple_volume<float>(wrt,src);
+    } else {
+      save_chist(output_f.c_str(),src_levels_s,src_pct_s);
     }
-    //provide mapping for upper range
-    
-    src_levels_s.push_back(src_max);
-    trg_levels_s.push_back(trg_max);
-    
-    if(verbose)
-      std::cout<<"[ max ] "<<src_levels_s[src_levels_s.size()-1]<<" => "<<trg_levels_s[trg_levels_s.size()-1]<<std::endl;
-    
-    if(verbose) 
-      std::cout<<"Recalculating intensities..."<<std::flush;
-    
-    for(int i=0;i<src.c_buf_size();i++)
-    {
-      //use LUT to map the intensities
-      int bin;
-      double input=src.c_buf()[i];
-      
-      double output=input;
-      for(bin=0;bin<src_levels_s.size();bin++)
-        if(input <= src_levels_s[bin]) break;
-      
-      if(bin==0) // first bin ?
-        output=trg_levels_s[0];
-      else if(bin>=(src_levels_s.size()-1))  
-        output=trg_levels_s[trg_levels_s.size()-1];
-      else 
-        output=(input-src_levels_s[bin-1])/(src_levels_s[bin]-src_levels_s[bin-1])*(trg_levels_s[bin]-trg_levels_s[bin-1])+trg_levels_s[bin-1];
-      
-      src.c_buf()[i]=output;
-    }
-    
-    if(verbose) 
-      std::cout<<"Done!"<<std::endl;
-    
-    minc_1_writer wrt;
-    
-    wrt.open(output_f.c_str(),rdr1);
-    wrt.append_history(history.c_str());
-    
-    save_simple_volume<float>(wrt,src);
-    
   } catch (const minc::generic_error & err) {
     std::cerr << "Got an error at:" << err.file () << ":" << err.line () << std::endl;
     std::cerr << err.msg()<<std::endl;
