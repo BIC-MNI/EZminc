1a3cf1feff866adde9725ecb7ba24194fa73d390
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2013-04-24 14:12:11 -0600

    Trying compile on Mac

--- config.h.cmake
+++ config.h.cmake
@@ -6,3 +6,9 @@
 
 /* do we have MINC1 API */
 #cmakedefine HAVE_MINC1 1
+
+/* Define to 1 if you have the `finite' function. */
+#cmakedefine HAVE_FINITE 1
+
+/* Define to 1 if you have the `isfinite' function. */
+#cmakedefine HAVE_ISFINITE 1
