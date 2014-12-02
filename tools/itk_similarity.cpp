5e71036f6df0783f7c10a3eeb2d3e21953d81956
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2014-12-02 12:24:21 -0500

    Fixed itk_similarity

--- tools/itk_similarity.cpp
+++ tools/itk_similarity.cpp
@@ -16,20 +16,22 @@
 #include <iostream>
 #include <itkImageMaskSpatialObject.h>
 // cost functions
-#include <itkMeanSquaresImageToImageMetric.h>
+
 #include <itkMattesMutualInformationImageToImageMetric.h>
 #include <itkMutualInformationHistogramImageToImageMetric.h>
-#include <itkMeanSquaresHistogramImageToImageMetric.h>
+#include <itkMutualInformationImageToImageMetric.h>
+#include <itkMeanSquaresImageToImageMetric.h>
 #include <itkCorrelationCoefficientHistogramImageToImageMetric.h>
 #include <itkNormalizedMutualInformationHistogramImageToImageMetric.h>
-#include <itkMutualInformationImageToImageMetric.h>
+#include <itkNormalizedCorrelationImageToImageMetric.h>
+#include <itkKullbackLeiblerCompareHistogramImageToImageMetric.h>
+#include <itkNormalizedCorrelationImageToImageMetric.h>
 
 #include <itkNearestNeighborInterpolateImageFunction.h>
 #include <itkKappaStatisticImageToImageMetric.h>
 
 #include <itkResampleImageFilter.h>
-//#include <itkIdentityTransform.h>
-#include <itkAffineTransform.h>
+#include <itkIdentityTransform.h>
 
 #include <itkImageFileReader.h>
 
@@ -49,45 +51,57 @@ using namespace  std;
 template <class T> typename T::Pointer load_file(const char *file)
 {
   typename itk::ImageFileReader<T>::Pointer reader = itk::ImageFileReader<T>::New();
-  
   reader->SetFileName(file);
   reader->Update();
   
-  return reader->GetOutput();
+  typename T::Pointer r=reader->GetOutput();
+  return r;
 }
 
 
 void show_usage (const char *name)
 {
   std::cerr 
-	  << "Usage: "<<name<<" <src> <target>" << endl
-    << "--src_mask <source_mask> " << endl
-    << "--target_mask <mask> "<< endl
+    << "Usage: "<<name<<" <src> <target>" << endl
+    << "--verbose print metric name"      << endl
+    << "--src_mask <source_mask> "        << endl
+    << "--target_mask <mask> "            << endl
     << "--mi mutual information"<< endl
     << "--nmi normalized mutual information "<<endl
     << "--mmi Mates Mutual information"<<endl
+    //<< "--kl Kullback Leibler histogram similarity"<<endl
     << "--msq mean squares "<<endl
-    << "--ccoeff Correlation coeffecient"<<endl
-    << "--kappa Kappa measure of similarity (binary images)"<<endl;
+    << "--cc correlation coeffecient"<<endl
+    << "--ncc Normalized correlation coeffecient"<<endl
+    << "--kappa Kappa measure of similarity (binary images)"<<endl
+    << "--bins <n> set number of histogram bins (per dimension)"<<endl;
 }
 
 int main (int argc, char **argv)
 {
-  int verbose=1;
-  int mi=0,nmi=0,mmi=0,msq=0,ccoeff=0;
-  int kappa=0;
+  int verbose=0;
+  int mi=0,nmi=0,mmi=0,msq=0,ccoeff=0,kl=0,ncc=0;
+  
+  int  kappa=0;
   bool binary=false;
+  int  hist_bins=64;
+  
   static struct option long_options[] = { 
-		{"src_mask",  required_argument, 0, 's'},
+    {"src_mask",  required_argument, 0, 's'},
     {"target_mask",  required_argument, 0, 't'},
-    {"mi",  no_argument, &mi, 1},
+    {"bins",  required_argument, 0, 'b'},
+    {"mi",   no_argument, &mi, 1},
     {"nmi",  no_argument, &nmi, 1},
     {"msq",  no_argument, &msq, 1},
     {"mmi",  no_argument, &mmi, 1},
     {"ccoeff",  no_argument, &ccoeff, 1},
+    {"cc",  no_argument, &ccoeff, 1},
+    {"ncc",  no_argument, &ncc, 1},
+    {"kl",  no_argument, &kl, 1},
     {"kappa",  no_argument, &kappa, 1},
-		{0, 0, 0, 0}
-		};
+    {"verbose",  no_argument, &verbose, 1},
+    {0, 0, 0, 0}
+  };
     
   std::string src,target,src_mask,target_mask;
   
@@ -98,23 +112,24 @@ int main (int argc, char **argv)
       if (c == -1) break;
       
       switch (c)
-			{
-			case 0:
-				break;
+      {
+      case 0:
+              break;
       case 's':
         src_mask = optarg;
         break;
       case 't':
         target_mask = optarg;
         break;
-			case '?':
-				/* getopt_long already printed an error message. */
-        //cerr<<"Unknown option:"<<optarg<<endl;
-			default:
-				show_usage (argv[0]);
-				return 1;
-			}
-  }
+      case 'b':
+        hist_bins=atoi(optarg);
+        break;
+      case '?':
+      default:
+              show_usage (argv[0]);
+              return 1;
+      }
+    }
 	if ((argc - optind) < 2) {
 		show_usage(argv[0]);
 		return 1;
@@ -134,21 +149,24 @@ int main (int argc, char **argv)
     typedef itk::Image<FloatVoxel, 3>          FloatImage;
     typedef itk::Image<BinaryVoxel, 3>         BinaryImage;
     
-    typedef itk::MattesMutualInformationImageToImageMetric<FloatImage,FloatImage> MatesMetric;
-    typedef itk::ImageToImageMetric< FloatImage, FloatImage> FloatMetricBase;
+    typedef itk::ImageToImageMetric< FloatImage,   FloatImage>   FloatMetricBase;
     typedef itk::ImageToImageMetric< BinaryImage, BinaryImage>   BinaryMetricBase;
+
+    // sign for metric - to make sure all metrics show higher value when similarity is maximized
+    int sign=1;
     
     if(binary) // treat input images as binary masks
     {
       BinaryMetricBase::Pointer metric;
-      if(kappa) {
+      //if(kappa) {
         itk::KappaStatisticImageToImageMetric<BinaryImage,BinaryImage>::Pointer  
             kmetric=itk::KappaStatisticImageToImageMetric<BinaryImage,BinaryImage>::New();
         kmetric->SetForegroundValue(1);
         metric=kmetric;
-        
-      }
+        if(verbose) cout<<"kappa=";
+      //}
       metric->ComputeGradientOff();
+      sign=1;
       
       BinaryImage::Pointer img_src, img_trg;
       img_src=load_file<BinaryImage>(src.c_str());
@@ -159,13 +177,13 @@ int main (int argc, char **argv)
       BinaryImage::Pointer    src_mask_img( BinaryImage::New() ),
       trg_mask_img( BinaryImage::New() );
 
-      typedef itk::NearestNeighborInterpolateImageFunction< BinaryImage,double> NearestNeighborInterpolator;
-      typedef itk::AffineTransform<double,3> AffineTransform;
+      typedef itk::NearestNeighborInterpolateImageFunction< BinaryImage,double> InterpolatorType;
+      typedef itk::IdentityTransform<double,3> TransformType;
       
-      AffineTransform::Pointer transform = AffineTransform::New();
-      transform->SetIdentity();
-      AffineTransform::ParametersType par=transform->GetParameters();
-      NearestNeighborInterpolator::Pointer interpolator=NearestNeighborInterpolator::New();
+      TransformType::Pointer transform = TransformType::New();
+      TransformType::ParametersType par=transform->GetParameters();
+      
+      InterpolatorType::Pointer interpolator=InterpolatorType::New();
       
       if(!target_mask.empty())
       {
@@ -180,36 +198,98 @@ int main (int argc, char **argv)
         src_mask_obj->SetImage( src_mask_img );
         metric->SetMovingImageMask( src_mask_obj  );
       }
+      metric->ComputeGradientOff();
+      metric->SetUseAllPixels(true);
       
       metric->SetMovingImage(img_src);
       metric->SetFixedImage (img_trg);
+      
       metric->SetFixedImageRegion( img_trg->GetLargestPossibleRegion() );
       interpolator->SetInputImage( img_src );
       
       metric->SetTransform( transform );
       metric->SetInterpolator( interpolator );
       metric->Initialize();
+      
       cout.precision(10);
-      cout<<metric->GetValue(par)<<endl;
+      cout<<metric->GetValue(par)*sign<<endl;
       
     } else {
       FloatMetricBase::Pointer metric;
+      
       if(ccoeff) {
-        metric=itk::CorrelationCoefficientHistogramImageToImageMetric<FloatImage,FloatImage>::New();
+        itk::CorrelationCoefficientHistogramImageToImageMetric<FloatImage,FloatImage>::Pointer m=
+        itk::CorrelationCoefficientHistogramImageToImageMetric<FloatImage,FloatImage>::New();
+        sign=1;
+
+        typedef itk::CorrelationCoefficientHistogramImageToImageMetric<FloatImage,FloatImage>::HistogramSizeType HistogramSizeType;
+        HistogramSizeType histogramSize;
+        histogramSize.SetSize(2);
+        histogramSize[0] = hist_bins;
+        histogramSize[1] = hist_bins;
+        m->SetHistogramSize( histogramSize );
+
+        metric=m;
+        if(verbose) cout<<"cc=";
       } else if(nmi) {
-        metric=itk::NormalizedMutualInformationHistogramImageToImageMetric<FloatImage,FloatImage>::New();
+        itk::NormalizedMutualInformationHistogramImageToImageMetric<FloatImage,FloatImage>::Pointer m=itk::NormalizedMutualInformationHistogramImageToImageMetric<FloatImage,FloatImage>::New();
+        sign=1;
+        
+        typedef itk::NormalizedMutualInformationHistogramImageToImageMetric<FloatImage,FloatImage>::HistogramSizeType HistogramSizeType;
+        HistogramSizeType histogramSize;
+        histogramSize.SetSize(2);
+        histogramSize[0] = hist_bins;
+        histogramSize[1] = hist_bins;
+        m->SetHistogramSize( histogramSize );
+
+        metric=m;
+        if(verbose) cout<<"nmi=";
       } else if(msq) {
         metric=itk::MeanSquaresImageToImageMetric<FloatImage,FloatImage>::New();
+        
       } else if(mmi) {
-        MatesMetric::Pointer m=MatesMetric::New();
+        itk::MattesMutualInformationImageToImageMetric<FloatImage,FloatImage>::Pointer m=itk::MattesMutualInformationImageToImageMetric<FloatImage,FloatImage>::New();
+        sign=-1;
+        
         m->SetUseAllPixels(true);
-        m->SetNumberOfHistogramBins(50);
+        m->SetNumberOfHistogramBins(hist_bins);
         
         metric=m;
+        if(verbose) cout<<"mmi=";
+      } else if(kl) {
+        itk::KullbackLeiblerCompareHistogramImageToImageMetric<FloatImage,FloatImage>::Pointer m=itk::KullbackLeiblerCompareHistogramImageToImageMetric<FloatImage,FloatImage>::New();
+
+        typedef itk::KullbackLeiblerCompareHistogramImageToImageMetric<FloatImage,FloatImage>::HistogramSizeType HistogramSizeType;
+        HistogramSizeType histogramSize;
+        histogramSize.SetSize(2);
+        histogramSize[0] = hist_bins;
+        histogramSize[1] = hist_bins;
+        m->SetHistogramSize( histogramSize );
+
+        metric=m;
+        if(verbose) cout<<"kl=";
+      } else if(ncc) {
+        itk::NormalizedCorrelationImageToImageMetric<FloatImage,FloatImage>::Pointer m=itk::NormalizedCorrelationImageToImageMetric<FloatImage,FloatImage>::New();
+
+        metric=m;
+        if(verbose) cout<<"ncc=";
       } else /* mi */{
-        metric=itk::MutualInformationHistogramImageToImageMetric<FloatImage,FloatImage>::New();
+        itk::MutualInformationHistogramImageToImageMetric<FloatImage,FloatImage>::Pointer m=itk::MutualInformationHistogramImageToImageMetric<FloatImage,FloatImage>::New();
+
+        typedef itk::MutualInformationHistogramImageToImageMetric<FloatImage,FloatImage>::HistogramSizeType HistogramSizeType;
+        HistogramSizeType histogramSize;
+        histogramSize.SetSize(2);
+        histogramSize[0] = hist_bins;
+        histogramSize[1] = hist_bins;
+        
+        m->SetHistogramSize( histogramSize );
+        
+        metric=m;
+        if(verbose) cout<<"mi=";
       }
+      
       metric->ComputeGradientOff();
+      metric->SetUseAllPixels(true);
       
       FloatImage::Pointer img_src,img_trg;
                        
@@ -219,16 +299,19 @@ int main (int argc, char **argv)
       
       MaskType::Pointer  src_mask_obj( MaskType::New()),
                          trg_mask_obj( MaskType::New());
-      BinaryImage::Pointer    src_mask_img( BinaryImage::New() ),
-                         trg_mask_img( BinaryImage::New() );
 
-      typedef itk::NearestNeighborInterpolateImageFunction< FloatImage,double> NearestNeighborInterpolator;
+      BinaryImage::Pointer    src_mask_img( BinaryImage::New() ),
+                              trg_mask_img( BinaryImage::New() );
+
+      typedef itk::NearestNeighborInterpolateImageFunction< FloatImage,double> InterpolatorType;
       typedef itk::AffineTransform<double,3> AffineTransform;
       
-      AffineTransform::Pointer transform = AffineTransform::New();
+      typedef itk::IdentityTransform<double,3> TransformType;
+      
+      TransformType::Pointer transform = TransformType::New();
       transform->SetIdentity();
-      AffineTransform::ParametersType par=transform->GetParameters();
-      NearestNeighborInterpolator::Pointer interpolator=NearestNeighborInterpolator::New();
+      TransformType::ParametersType par=transform->GetParameters();
+      InterpolatorType::Pointer interpolator=InterpolatorType::New();
       
       if(!target_mask.empty())
       {
@@ -246,14 +329,16 @@ int main (int argc, char **argv)
       
       metric->SetMovingImage(img_src);
       metric->SetFixedImage (img_trg);
+      
       metric->SetFixedImageRegion( img_trg->GetLargestPossibleRegion() );
       interpolator->SetInputImage( img_src );
       
       metric->SetTransform( transform );
       metric->SetInterpolator( interpolator );
       metric->Initialize();
+      
       cout.precision(10);
-      cout<<metric->GetValue(par)<<endl;
+      cout<<metric->GetValue(par)*sign<<endl;
   //    cerr<<metric<<endl;
     }
       
@@ -272,4 +357,6 @@ int main (int argc, char **argv)
   } 
   return 0;
 }  
+
+// kate: space-indent on; indent-width 2; indent-mode C++;replace-tabs on;tab-width 2
 
